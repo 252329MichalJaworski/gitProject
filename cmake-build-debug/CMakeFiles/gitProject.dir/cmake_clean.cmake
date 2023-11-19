5d303c3fb1f5e7f2d3cd0ff23a1f12077c212b70
Michał Jaworski <252329@edu.p.lodz.pl>
2023-11-19 13:06:19 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/gitProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitProject.dir/main.cpp.o"
+  "gitProject"
+  "gitProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
